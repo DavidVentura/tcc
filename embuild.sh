cf7d0365b55e1026ebd6983f77ab1e7e35e975d7
David Ventura <dventura@chorus.one>
2025-12-26 15:06:04 +0100

    Add example to syntax check file & wasm build

---
+++ embuild.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+CC="${CC:-emcc}"
+CFLAGS="-DTCC_SYNTAX_ONLY -DTCC_TARGET_X86_64 -DPTR_SIZE=8 -DLONG_SIZE=8 -O3 -DONE_SOURCE=1"
+LDFLAGS="-sINITIAL_MEMORY=33554432 -sEXPORTED_RUNTIME_METHODS=ccall -sEXPORTED_FUNCTIONS=_check_syntax,_init_tcc -sENVIRONMENT=worker -sEXIT_RUNTIME=0 -sSTRICT=1"
+OUTPUT="syntax_check.js"
+WARNS="-Wno-string-plus-int -Wno-undefined-internal"
+
+rm -f $OUTPUT syntax_check.wasm
+
+$CC -sEVAL_CTORS -sWASM_BIGINT -flto $WARNS $CFLAGS $LDFLAGS -o $OUTPUT syntax_check.c libtcc.c tccpp.c tccgen.c tcc-stubs.c -I. --preload-file em_include@/
---
+++ syntax_check_file.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "libtcc.h"
+
+void error_callback(void *opaque, const char *msg)
+{
+    fprintf(stderr, "%s\n", msg);
+}
+
+int check_file_syntax(const char *filename)
+{
+    TCCState *s = tcc_new();
+    if (!s) {
+        fprintf(stderr, "Could not create tcc state\n");
+        return -1;
+    }
+
+    tcc_set_lib_path(s, ".");
+    tcc_set_error_func(s, NULL, error_callback);
+    tcc_set_output_type(s, TCC_OUTPUT_MEMORY);
+    //tcc_set_output_type(s, TCC_OUTPUT_PREPROCESS);
+
+    int result = tcc_add_file(s, filename);
+
+    tcc_delete(s);
+
+    return result;
+}
+
+int check_syntax(const char *content)
+{
+    TCCState *s = tcc_new();
+    if (!s) {
+        fprintf(stderr, "Could not create tcc state\n");
+        return -1;
+    }
+
+    tcc_set_lib_path(s, ".");
+    tcc_set_error_func(s, NULL, error_callback);
+    tcc_set_output_type(s, TCC_OUTPUT_MEMORY);
+
+    int result = tcc_compile_string(s, content);
+
+    tcc_delete(s);
+
+    return result;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <file.c>\n", argv[0]);
+        return 1;
+    }
+
+    for(int i =0; i<100; i++) {
+	    int result = check_file_syntax(argv[1]);
+
+	    if (result == 0) {
+		    printf("Syntax OK: %s\n", argv[1]);
+	    } else {
+		    printf("Syntax errors in: %s\n", argv[1]);
+	    }
+    }
+}
